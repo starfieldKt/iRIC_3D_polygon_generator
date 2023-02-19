97c52abc0af7eee65ab546a32098041b9b0844ca
starfieldsKt <keita.hoshino.1001@gmail.com>
2023-02-19 20:41:21 +0900

    翻訳ファイルを追加

---
+++ install/translation_ja_JP.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>3D_polygon_generator</source>
+            <translation>3D_polygon_generator</translation>
+        </message>
+        <message>
+            <source>Basic Setting</source>
+            <translation>基本設定</translation>
+        </message>
+        <message>
+            <source>CGNS File Name</source>
+            <translation>CGNSファイル名</translation>
+        </message>
+        <message>
+            <source>Save Folder</source>
+            <translation>保存場所</translation>
+        </message>
+        <message>
+            <source>Save File Name</source>
+            <translation>保存するファイル名</translation>
+        </message>
+        <message>
+            <source>Output Stl</source>
+            <translation>stlを出力する</translation>
+        </message>
+        <message>
+            <source>Output obj</source>
+            <translation>objを出力する</translation>
+        </message>
+        <message>
+            <source>Z Scale</source>
+            <translation>z方向の倍率</translation>
+        </message>
+        <message>
+            <source>Output Start Time</source>
+            <translation>出力開始時間</translation>
+        </message>
+        <message>
+            <source>Oitput End Time</source>
+            <translation>出力終了時間</translation>
+        </message>
+        <message>
+            <source>Elevation Name</source>
+            <translation>河床の標高の名前</translation>
+        </message>
+        <message>
+            <source>Add Water Surface</source>
+            <translation>水面の出力をする</translation>
+        </message>
+        <message>
+            <source>Water surface elevation Name</source>
+            <translation>水面の標高の名前</translation>
+        </message>
+        <message>
+            <source>Elevation</source>
+            <translation></translation>
+        </message>
+    </context>
+</TS>
