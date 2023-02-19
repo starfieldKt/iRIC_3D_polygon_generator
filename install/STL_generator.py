23cefd8392a9cdc5ccbca65b4ba021e1d6c7352c
starfieldsKt <keita.hoshino.1001@gmail.com>
2023-02-19 18:45:23 +0900

    objを出力できるように更新、水面を表示できるように修正

--- install/STL_generator.py
+++ install/STL_generator.py
@@ -46,19 +46,39 @@ def cgns_close():
 # --------------------------------------------------
 def read_initial_condition():
 
-    global n_step_read, elevation_scale, elevation_name, save_location, n_cell
+    global n_step_read, elevation_scale, save_location, save_file_name, n_cell
     global start_time_output, end_time_output
+    global output_stl, output_obj
+    global bottom_elevation_name
+    global add_water_surface, ws_elevation_name
     global n_node_x, n_node_y, coordinate_x, coordinate_y
-    global coordinate_x_1d, coordinate_y_1d, coordinate_x_2d, coordinate_y_2d
+    global coordinate_x_1d, coordinate_y_1d
 
     # 読み込み用CGNSの計算ステップ数
     n_step_read = iric.cg_iRIC_Read_Sol_Count(read_cgns_id)
     # 3Dモデルを出力を終了する時間
     elevation_scale = iric.cg_iRIC_Read_Real(write_cgns_id, "elevation_scale")
+
     # 読み込み用CGNSの標高のデータ名
-    elevation_name = iric.cg_iRIC_Read_String(write_cgns_id, "elevation_name")
+    bottom_elevation_name = iric.cg_iRIC_Read_String(
+        write_cgns_id, "bottom_elevation_name"
+    )
+
+    # 水面を追加するか
+    add_water_surface = iric.cg_iRIC_Read_Integer(write_cgns_id, "add_water_surface")
+    # 読み込み用CGNSの水面標高のデータ名
+    ws_elevation_name = iric.cg_iRIC_Read_String(write_cgns_id, "ws_elevation_name")
+
+    # stlを出力するか
+    output_stl = iric.cg_iRIC_Read_Integer(write_cgns_id, "output_stl")
+    # objを出力するか
+    output_obj = iric.cg_iRIC_Read_Integer(write_cgns_id, "output_obj")
+
     # ファイルの保存場所
     save_location = iric.cg_iRIC_Read_String(write_cgns_id, "save_location")
+    # 保存するファイル名
+    save_file_name = iric.cg_iRIC_Read_String(write_cgns_id, "save_file_name")
+
     # 3Dモデルを出力を開始する時間
     start_time_output = iric.cg_iRIC_Read_Real(write_cgns_id, "start_time_output")
     # 3Dモデルを出力を終了する時間
@@ -74,83 +94,268 @@ def read_initial_condition():
     coordinate_x_1d = np.array(coordinate_x)
     coordinate_y_1d = np.array(coordinate_y)
 
-    # CGNSから読み込んだcoordinate_x_1dとcoordinate_y_1dは1次元配列なので2次元配列にする
+    # CGNSから読み込んだcoordinate_x_1dとcoordinate_y_1dは1次元配列なので2次元配列にする(デバッグ用)
     coordinate_x_2d = np.array(coordinate_x_1d).reshape(n_node_y, n_node_x)
     coordinate_y_2d = np.array(coordinate_y_1d).reshape(n_node_y, n_node_x)
 
 
 # --------------------------------------------------
-# ポリゴンの頂点の座標(x,y,z)を3次元配列として作成する。
+# 河床の水面の標高を読み込む。(河床)
 # --------------------------------------------------
-def make_polygon_node_coordinate_list():
+def read_elevation_vale_bottom():
 
-    global elevation_vale_1d, elevation_vale_2d, polygon_node_list
+    global bottom_elevation_vale_1d
 
-    elevation_vale = iric.cg_iRIC_Read_Sol_Node_Real(read_cgns_id, step, elevation_name)
-    # CGNSから読み込んだelevation_valeをnumpy配列にする。
-    elevation_vale_1d = np.array(elevation_vale) * elevation_scale
-    # CGNSから読み込んだelevation_valeは1次元配列なので2次元配列にする
-    elevation_vale_2d = np.array(elevation_vale_1d).reshape(n_node_y, n_node_x)
-
-    polygon_node_list = np.stack(
-        [coordinate_x_1d, coordinate_y_1d, elevation_vale_1d], 1
+    bottom_elevation_vale = iric.cg_iRIC_Read_Sol_Node_Real(
+        read_cgns_id, step, bottom_elevation_name
     )
+    # CGNSから読み込んだelevation_valeをnumpy配列にする。
+    bottom_elevation_vale_1d = np.array(bottom_elevation_vale) * elevation_scale
+
+    # CGNSから読み込んだelevation_valeは1次元配列なので2次元配列にする(デバッグ用)
+    bottom_elevation_vale_2d = np.array(bottom_elevation_vale_1d).reshape(
+        n_node_y, n_node_x
+    )
+
+
+# --------------------------------------------------
+# 水面の標高を読み込む。(水面)
+# --------------------------------------------------
+def read_elevation_vale_ws():
+
+    global ws_elevation_vale_1d
+
+    ws_elevation_vale = iric.cg_iRIC_Read_Sol_Node_Real(
+        read_cgns_id, step, ws_elevation_name
+    )
+    # CGNSから読み込んだws_elevation_valeをnumpy配列にする。
+    ws_elevation_vale_1d = np.array(ws_elevation_vale) * elevation_scale
+
+    for i in range(n_node_x * n_node_y):
+        if ws_elevation_vale_1d[i] <= bottom_elevation_vale_1d[i]:
+            ws_elevation_vale_1d[i] = bottom_elevation_vale_1d[i]
+
+    # CGNSから読み込んだelevation_valeは1次元配列なので2次元配列にする(デバッグ用)
+    ws_elevation_vale_2d = np.array(ws_elevation_vale_1d).reshape(n_node_y, n_node_x)
+
+
+def get_v(x_coords, y_coords, z_coords):
+    """
+    x, y, z座標値を含む二次元配列からobjの頂点情報を取得する。\n
+    Args:
+        x_coords : x座標の一次元配列
+        y_coords : y座標の一次元配列
+        z_coords : z座標の一次元配列
+    Returns:
+        v_text : obj形式の頂点情報
+    """
+
+    v_text = ""
+
+    # テキスト情報として取得(objはyが高さ方向)
+    for i in range(x_coords.shape[0]):
+        v_text += (
+            "v "
+            + str(x_coords[i])
+            + " "
+            + str(z_coords[i])
+            + " "
+            + str(y_coords[i])
+            + "\n"
+        )
+
+    return v_text
+
+
+def get_vt(x_coords, y_coords):
+    """
+    x,y座標値を含む二次元配列からテクスチャ座標情報を取得する。\n
+    Args:
+        x_coords : x座標の二次元配列
+        y_coords : y座標の二次元配列
+    Returns:
+        vt_text : obj形式のテクスチャ座標情報
+    """
+    # 戻り値の定義
+    vt_text = ""
+
+    # 最大値&最小値の取得
+    x_min = min(x_coords)
+    x_max = max(x_coords)
+    y_min = min(y_coords)
+    y_max = max(y_coords)
+
+    for i in range(x_coords.shape[0]):
+        # テクスチャ座標
+        xt = (x_coords[i] - x_min) / (x_max - x_min)
+        yt = (y_coords[i] - y_min) / (y_max - y_min)
+
+        # テクスチャ座標の定義
+        vt_text += "vt " + str(xt) + " " + str(yt) + "\n"
+
+    return vt_text
+
+
+def get_f(triangle_configuration_node_list):
+    """
+    格子形状からobjのメッシュ情報を取得する。\n
+    Args:
+        ni : 縦断方向の分割数
+        nj : 横断方向の分割数
+    Returns:
+        f_text : obj形式のメッシュ情報
+    """
+    # 戻り値の定義
+    f_text = ""
+
+    for i in range(triangle_configuration_node_list.shape[0]):
+
+        f_text += (
+            "f "
+            + str(triangle_configuration_node_list[i][0] + 1)
+            + "/"
+            + str(triangle_configuration_node_list[i][0] + 1)
+            + "/"
+            + "1"
+            + " "
+            + str(triangle_configuration_node_list[i][1] + 1)
+            + "/"
+            + str(triangle_configuration_node_list[i][1] + 1)
+            + "/"
+            + "1"
+            + " "
+            + str(triangle_configuration_node_list[i][2] + 1)
+            + "/"
+            + str(triangle_configuration_node_list[i][2] + 1)
+            + "/"
+            + "1"
+            + "\n"
+        )
+
+    return f_text
 
 
 # --------------------------------------------------
 # 三角形分割
 # --------------------------------------------------
-def make_tryangle():
+def make_tryangle(previous_count):
 
-    global triangle_configuration_node_list
+    triangle_configuration_node_list_tmp = np.full((n_cell * 2, 3), 0)
 
     for i in range(n_node_x - 1):
         for j in range(n_node_y - 1):
 
             tryangle_tmp_1 = np.array(
                 [
-                    i + j * n_node_x,
-                    (i + 1) + j * n_node_x,
-                    (i + 1) + (j + 1) * n_node_x,
+                    i + j * n_node_x + previous_count,
+                    (i + 1) + (j + 1) * n_node_x + previous_count,
+                    (i + 1) + j * n_node_x + previous_count,
                 ],
                 dtype=np.int64,
             )
             tryangle_tmp_2 = np.array(
                 [
-                    i + j * n_node_x,
-                    (i + 1) + (j + 1) * n_node_x,
-                    i + (j + 1) * n_node_x,
+                    i + j * n_node_x + previous_count,
+                    i + (j + 1) * n_node_x + previous_count,
+                    (i + 1) + (j + 1) * n_node_x + previous_count,
                 ],
                 dtype=np.int64,
             )
 
-            triangle_configuration_node_list[
+            triangle_configuration_node_list_tmp[
                 (i + j * (n_node_x - 1)) * 2
             ] = tryangle_tmp_1
-            triangle_configuration_node_list[
+            triangle_configuration_node_list_tmp[
                 (i + j * (n_node_x - 1)) * 2 + 1
             ] = tryangle_tmp_2
+
+    return triangle_configuration_node_list_tmp
 
 
 # --------------------------------------------------
 # stlファイルの作成
 # --------------------------------------------------
-def make_polygon_obj():
+def make_polygon_stl(elevation_vale_1d, triangle_configuration_node_list, bottom_or_ws):
 
-    global triangle_configuration_node_list
-    global polygon_node_list
+    polygon_node_list = np.stack(
+        [coordinate_x_1d, coordinate_y_1d, elevation_vale_1d], 1
+    )
 
     # この辺についてはよくわかっていない
     polygon_obj = mesh.Mesh(
         np.zeros(triangle_configuration_node_list.shape[0], dtype=mesh.Mesh.dtype)
     )
+    polygon_obj.remove_duplicate_polygons = True
+
     for i, f in enumerate(triangle_configuration_node_list):
         for j in range(3):
             polygon_obj.vectors[i][j] = polygon_node_list[f[j], :]
 
     polygon_obj.save(
-        save_location + "\\" + "polygon_" + str("{:.2f}".format(time)) + ".stl"
+        save_location
+        + "\\"
+        + save_file_name
+        + "_time="
+        + str("{:.2f}".format(time))
+        + "_"
+        + bottom_or_ws
+        + ".stl"
     )
+
+
+def make_polygon_obj(v_text, vt_text, f_text, bottom_or_ws):
+    # objファイル
+    with open(
+        save_location
+        + "\\"
+        + save_file_name
+        + "_time="
+        + str("{:.2f}".format(time))
+        + "_"
+        + bottom_or_ws
+        + ".stl"
+    ) as f:
+
+        # マテリアル定義ファイル
+        obj_text = "# mtl file" + "\n"
+        obj_text += (
+            "mtllib "
+            + save_file_name
+            + "_time="
+            + str("{:.2f}".format(time))
+            + "_"
+            + bottom_or_ws
+            + ".mtl"
+            + "\n"
+            + "\n"
+        )
+
+        # # グループ名
+        # obj_text += "# Group Name" + "\n"
+        # obj_text += "g step" + str(step) + "\n" + "\n"
+
+        # MAT
+        obj_text += "# Material Infomation" + "\n"
+        obj_text += "usemtl water" + "\n" + "\n"
+
+        # 格子点情報
+        obj_text += "# Node Coordinate" + "\n"
+        obj_text += v_text + "\n"
+
+        # テクスチャ座標情報
+        obj_text += "# Texture Coordinate" + "\n"
+        obj_text += vt_text + "\n"
+
+        # 垂直方向情報
+        obj_text += "# Vertical Infomation" + "\n"
+        obj_text += "vn 0.0 1.0 0.0" + "\n" + "\n"
+
+        # メッシュ情報
+        obj_text += "# Mesh Infomaition" + "\n"
+        obj_text += f_text
+
+        # 書き込み
+        f.write(obj_text)
 
 
 if __name__ == "__main__":
@@ -163,10 +368,12 @@ if __name__ == "__main__":
 
     write_cgns_name = sys.argv[1]
 
+    # write_cgns_name = (
+    #     "C:\WorkSpace\iRIC\iRICver4\project\stl_generator_test\stl_gen_4\Case1.cgn"
+    # )
+
     cgns_open()
     read_initial_condition()
-
-    triangle_configuration_node_list = np.full((n_cell * 2, 3), 0)
 
     for step_tmp in range(n_step_read):
 
@@ -179,8 +386,39 @@ if __name__ == "__main__":
         # start_time_outputからend_time_outputまで繰り返し出力
         if time >= start_time_output and time <= end_time_output:
 
-            make_polygon_node_coordinate_list()
-            make_tryangle()
-            make_polygon_obj()
+            read_elevation_vale_bottom()
+            triangle_configuration_node_list_bottom = make_tryangle(0)
+
+            if output_stl == 1:
+                make_polygon_stl(
+                    bottom_elevation_vale_1d,
+                    triangle_configuration_node_list_bottom,
+                    "bottom",
+                )
+
+            if output_obj == 1:
+                v_text = get_v(
+                    coordinate_x_1d, coordinate_y_1d, bottom_elevation_vale_1d
+                )
+                vt_text = get_vt(coordinate_x_1d, coordinate_y_1d)
+                f_text = get_f(triangle_configuration_node_list_bottom)
+                make_polygon_obj(v_text, vt_text, f_text, "bottom")
+
+            if add_water_surface == 1:
+                read_elevation_vale_ws()
+                triangle_configuration_node_list_ws = make_tryangle(0)
+
+                if output_stl == 1:
+                    make_polygon_stl(
+                        ws_elevation_vale_1d, triangle_configuration_node_list_ws, "ws"
+                    )
+
+                if output_obj == 1:
+                    v_text = get_v(
+                        coordinate_x_1d, coordinate_y_1d, ws_elevation_vale_1d
+                    )
+                    vt_text = get_vt(coordinate_x_1d, coordinate_y_1d)
+                    f_text = get_f(triangle_configuration_node_list_ws)
+                    make_polygon_obj(v_text, vt_text, f_text, "ws")
 
     cgns_close()
